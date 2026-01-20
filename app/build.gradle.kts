8197675670bafd2073e4649156ff3f2a2dac0160
cainan <csoliveira01@gmail.com>
2026-01-19 21:08:10 -0400

    Mapping redis server

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,5 +16,7 @@ dependencies {
     implementation(libs.spring.boot.starter.security)
 
     implementation(libs.spring.boot.starter.data.jpa)
+    implementation(libs.spring.boot.starter.data.redis)
+
     runtimeOnly(libs.postgresql)
 }
